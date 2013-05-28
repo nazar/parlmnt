3902f2699bcbd60bed8491386c223b76fd4e5a6f
Nazar Aziz <nazar@panthersoftware.com>
2013-05-28 14:59:17 +0100

    * eager load constituencies for MPs

--- app/controllers/mps_controller.rb
+++ app/controllers/mps_controller.rb
@@ -1,7 +1,7 @@
 class MpsController < SponsorsController
 
   def index
-    mps = Sponsor.mps.order('name').includes([:party])
+    mps = Sponsor.mps.order('name').includes([:party, :constituency])
     json_responder(mps, :each_serializer => SponsorIndexSerializer)
   end
 
