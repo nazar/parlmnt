305c08bd5985465ec94daac71385d530c2862e9c
Nazar Aziz <nazar@panthersoftware.com>
2013-06-03 11:35:54 +0100

    * navbar controller to directive * allow global search box to search for constituencies

--- app/assets/javascripts/app/controllers/navbar_controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('parlmntDeps').controller('navbarController', ['$scope', '$location', function($scope, $location) {
-
-  $scope.navClass = function (page) {
-    var currentRoute = $location.path().substring(1) || 'home';
-    return page === currentRoute ? 'active' : '';
-  };
-
-}]);
--- app/models/constituency.rb
+++ app/models/constituency.rb
@@ -4,7 +4,7 @@ class Constituency < ActiveRecord::Base
   validates :name, :uniqueness => true
   validates :name, :presence => true
 
-  has_many :sponsors
   has_many :users
+  has_one :sponsor
 
 end
